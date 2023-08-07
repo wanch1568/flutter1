d7c7e9ae00429aeb07ce77a56f2cae3c98f022ec
mta <54351211+wanch1568@users.noreply.github.com>
2023-08-07 12:49:25 +0900

    Update test.dart

--- test.dart
+++ test.dart
@@ -1,4 +1,46 @@
-class InputCalender extends ConsumerWidget{
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import '/view_models/select_date.dart';
+import '/provider/riverpod_provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MyTableCalendar extends ConsumerWidget{
+    bool isChanged=false;
+    DateTime _focusedDay = DateTime.now();
+    DateTime? _selectedDay;
+    int num=0;
+    @override 
+    Widget build(BuildContext context,WidgetRef ref){
+        _focusedDay=ref.watch(selectedDate);
+        _selectedDay=ref.watch(selectedDate);
+        //ref.watch(calenderChanged);
+        num+=1;
+        print(num);
+        print("changed!");
+        return TableCalendar(
+            locale: 'ja_JP',
+            shouldFillViewport:true,
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+                //print(day.toString()+isSameDay(_selectedDay, day).toString());
+                return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+                _selectedDay=selectedDay;
+                _focusedDay=focusedDay;
+                print(selectedDay);
+                ref.read(selectedDate.notifier).state=selectedDay;
+                print(focusedDay);
+            },
+            
+        );
+    }
+}
+
+/*class InputCalender extends ConsumerWidget{
     bool isChanged=false;
     DateTime _focusedDay = DateTime.now();
     DateTime? _selectedDay=DateTime.now();
@@ -55,4 +97,4 @@ class InputCalender extends ConsumerWidget{
         );
         
     }
-}
+}*/
